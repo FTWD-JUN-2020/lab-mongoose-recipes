e68a6ae0e3bf55af38cfc123141abee0729e13b9
Cule219 <culaficstefan@gmail.com>
2020-07-22 18:52:41 -0400

    done

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    const response = await Recipe.create(data[0]);
+    // console.log(response);
+    console.log("heya there this comes after the recipe");
+    const response2 = await Recipe.insertMany(data);
+    // console.log(response2);
+    const response3 = await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+    // console.log(response3);
+    const response4 = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log(response4);
+    mongoose.connection.close();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// .then notation
+// mongoose
+// .connect(MONGODB_URI, {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then((self) => {
+//   console.log(`Connected to the database: "${self.connection.name}"`);
+//   // Before adding any documents to the database, let's delete all previous entries
+//   return self.connection.dropDatabase();
+// })
+// .then(() => {
+//   // Run your code here, after you have insured that the connection was made
+//   Recipe.create(data[0]).then((response) => {
+//     console.log(response);
+//     console.log("heya there this comes after the recipe");
+//   });
+// })
+// .catch((error) => {
+//   console.error("Error connecting to the database", error);
+// });
